343d586a451d63492d12d4a4701359b93cb66407
Minhee Han <yyj9694651@gmail.com>
2025-11-28 16:42:51 +0900

    Feat: add PostController for managing post creation and validation

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -12,7 +12,6 @@ public class Post extends BaseEntity {
     private String title;
     private String content;
 
-
     public Post(String title, String content) {
         this.title = title;
         this.content = content;
--- src/main/java/com/back/global/entity/BaseEntity.java
+++ src/main/java/com/back/global/entity/BaseEntity.java
@@ -1,6 +1,10 @@
 package com.back.global.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +15,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
